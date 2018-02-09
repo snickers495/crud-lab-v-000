b9b9ee23dd8e6e6383c2b59d3fc6a2de1938afe8
Lisa Lee <snickers495@gmail.com>
2018-02-09 18:20:19 +0000

    automatically backed up by learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import cuid from 'cuid';
 
 class RestaurantInput extends Component {
   render() {
     return (
       <div>
-        Restaurant Input
+        
       </div>
     );
   }
